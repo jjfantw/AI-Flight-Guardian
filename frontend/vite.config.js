4ef50cb6ceb09186a6a5f1f4e3539738ab316241
jjfantw <41936098+jjfantw@users.noreply.github.com>
2026-04-22 09:25:53 +0800

    fix: trigger github actions for pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Base path according to repo name for GitHub Pages
   base: '/AI-Flight-Guardian/',
   plugins: [react()],
 })
